c0684914a9ca981501b79a1f15d5a7c5e429949d
lxry-comet <durdentagirowich@gmail.com>
2026-03-11 14:30:30 +0100

    onclick added

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -17,13 +17,30 @@ import css from './Counter.module.css';
 
 //* Компонент клас
 export class Counter extends Component {
-	render() {
+	    handleIncrement = () => {
+          console.log("+1");
+        };
+        handleDecrement = () => {
+          console.log("-1");
+        };
+    render() {
 		return (
 			<div className={css.counter}>
 				<span className={css.counterValue}>0</span>
 				<div className={css.counterControls}>
-					<button className={css.buttonIncrement}>+ 1</button>
-					<button className={css.buttonDecrement}>- 1</button>
+
+					<button 
+                        className={css.buttonIncrement}
+                        onClick={this.handleIncrement}>
+                    + 1
+                     </button>
+
+					<button
+                        className={css.buttonDecrement}
+                        onClick={this.handleDecrement}>
+                    - 1
+                     </button>
+
 				</div>
 			</div>
 		)
